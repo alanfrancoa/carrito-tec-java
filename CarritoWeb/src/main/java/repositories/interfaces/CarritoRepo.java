88b3260c2fdc71e91772d6a54ce1310d0bc0900b
MatiasCordich <m.scordich@gmail.com>
2024-11-28 00:43:18 -0300

    Se creo la Interfaz de carrito para el repo

---
+++ CarritoWeb/src/main/java/repositories/interfaces/CarritoRepo.java
@@ -0,0 +1,14 @@
+package repositories.interfaces;
+
+import java.util.List;
+
+import modelos.carrito.Carrito;
+import modelos.carrito.Renglon;
+
+public interface CarritoRepo {
+
+	void agregar(Renglon nuevoRenglon);
+	List<Renglon> verCarrito();
+	double verMontoTotal();
+	double finalizarCompra();
+}
